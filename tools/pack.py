27a1f553d21628c2ac69c87ef0c3adb27bb2a3d7
Gu_ZT <administration@ztxy666.cn>
2022-03-29 17:24:31 +0800

    ci

--- tools/pack.py
+++ tools/pack.py
@@ -1,21 +1,27 @@
 import json
+from os import mkdir
 import os.path
 from . import pro_dir
 from .lib import readConfig
 import zipfile
 
 
-def pack():
+def pack(args: str = 'null'):
     pack_mcmeta = os.path.join(pro_dir, 'pack.mcmeta')
     pmm = json.loads(open(pack_mcmeta, 'r+', encoding='utf-8').read())
     version = pmm['pack']['description'][1]['text']
     version = version[2:]
-    name = readConfig.getConfig('settings', 'pack_name').format(version=version, mcv=readConfig.getConfig('settings', 'mcv'))
-
+    name = os.path.join(pro_dir,"out",readConfig.getConfig('settings', 'pack_name').format(version=version, mcv=readConfig.getConfig('settings', 'mcv'))+".zip")
+    if args != 'null':
+        name = os.path.join(pro_dir,"out",readConfig.getConfig('settings', 'pack_name').format(version=version, mcv=readConfig.getConfig('settings', 'mcv'))+f"-{args}"+".zip")
+    out_dir = os.path.join(pro_dir, "out")
     data = os.path.join(pro_dir, 'data')
     assets = os.path.join(pro_dir, 'assets')
     pack_png = os.path.join(pro_dir, 'pack.png')
     license_f = os.path.join(pro_dir, 'LICENSE')
+
+    if not os.path.isdir(out_dir):
+        mkdir(out_dir)
 
     z = zipfile.ZipFile(name, 'w')
     z.write(pack_png, "pack.png")
