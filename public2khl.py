97bac898579dbca5b68d8eb297282188eb8d38a6
Huaji_MUR233 <huajimur@jvav.me>
2022-04-03 14:59:10 +0800

    .gitignore changes

--- public2khl.py
+++ public2khl.py
@@ -1,17 +1,23 @@
 import asyncio
 import sys
-from os import getenv
+from os import environ
 
 from khl import Bot, MessageTypes
+from khl.card import CardMessage, Card, Module
 
 
 def up(token: str, channel: str, file: str):
     bot = Bot(token=token)
 
     async def upload(_channel: str, _file: str):
-        await bot.send(content=await bot.create_asset(_file), target=await bot.fetch_public_channel(_channel), type=MessageTypes.FILE)
+        msg = CardMessage(Card(Module.File(type="file", src=await bot.create_asset(_file), title=_file[2:])))
+        await bot.send(content=msg, target=await bot.fetch_public_channel(_channel),
+                       type=MessageTypes.CARD)
     asyncio.get_event_loop().run_until_complete(upload(channel, file))
 
 
-args = sys.argv
-up(args[1], args[2], args[3])
+if __name__ == '__main__':
+    args = sys.argv
+    BOT_TOKEN = environ["BOT_TOKEN"]
+    CHANNEL_ID = environ["CHANNEL_ID"]
+    up(BOT_TOKEN, CHANNEL_ID, args[1])
